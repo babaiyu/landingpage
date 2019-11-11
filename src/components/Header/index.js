c647d8914e02e9a2ceec0700e320d49216f4b522
babaiyu <bayuputra9a@gmail.com>
2019-11-11 14:45:12 +0700

    added hero shot

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,12 +4,18 @@ import './style.css';
 
 const name = 'Bayu Permana';
 
+const btn = `LET'S MAKE CONTACT`;
+
 const Header = () => {
   return (
     <div className='contentHeader'>
-      <img src={logo} className='imgLogo' />
-      <div className="bodyHeader">
-        <p>Hello! I'm {name}</p>
+      <img src={logo} className='imgLogo' alt="Logo" />
+      <div className='bodyHeader'>
+        <h1>Hello! I'm {name}</h1>
+        <h2>Consult, Design, and Develop Websites</h2>
+        <p>Have something great in mind? Feel free to contact me.</p>
+        <p>I'll help you to make it happen.</p>
+        <button className="btnHeader" onClick={() => alert(btn)}>{btn}</button>
       </div>
     </div>
   );
